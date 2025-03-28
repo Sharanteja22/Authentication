ed7e491044a1c2a292d6a4d621bc7d8c32440677
sharanteja2211 <sharanteja2211@gmail.com>
2025-03-28 14:05:41 +0530

    eroor resolved in taileind css

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,6 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc'; // Ensure React plugin is used
 
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+});
