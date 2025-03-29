117784897135ee6eabcd0e064e5ca5a7cb120b39
sharanteja2211 <sharanteja2211@gmail.com>
2025-03-29 13:37:49 +0530

    sigin api done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-
+import jwt from "jsonwebtoken";
 export const signup=async (req,res,next)=>{
     const {username, email, password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10)
@@ -12,3 +12,24 @@ export const signup=async (req,res,next)=>{
         next(error);
     }
 }
+
+
+export const signin=async (req,res,next)=>{
+    const {email, password}=req.body;
+    try {
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+        const isPasswordValid=bcryptjs.compareSync(password,user.password);
+        if(!isPasswordValid){
+            return res.status(401).json({message: "Invalid credentials"});
+        }
+        const {password:hashedPassword, ...others}=user._doc;
+        const token=jwt.sign({id:user._id},process.env.JWT_SECRET);
+        const expiryDate=new Date(Date.now()+24*60*60*1000);
+        res.cookie('access_token',token,{httpOnly:true, expires:expiryDate}).status(200).json(others);  
+    } catch (error) {
+        next(error);
+    }
+}    
