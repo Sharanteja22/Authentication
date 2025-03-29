8849b39da016052aa3a8bf6ef340afdc39bf9bed
sharanteja2211 <sharanteja2211@gmail.com>
2025-03-29 13:15:23 +0530

    sign uop functionality completed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss(),react()
   ],
+  server  : { 
+      proxy:{
+        '/api':{
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        }
+      }
+   }
+
 })
