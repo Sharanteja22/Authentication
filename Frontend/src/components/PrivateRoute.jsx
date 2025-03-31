a0517cf70dc0d5a00f795f9876acaa025ce83e55
sharanteja2211 <sharanteja2211@gmail.com>
2025-03-31 21:27:14 +0530

    header updated

---
+++ Frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import {Outlet, Navigate} from 'react-router-dom'
+export default function PrivateRoute() {
+
+   const {currentUser}=useSelector((state) => state.user);
+    
+  return (
+    <div>
+      {currentUser ? (
+        <Outlet/>
+      ):(
+        <Navigate to="/sign-in"/>
+      ) 
+    }    
+    </div>
+  )
+}
