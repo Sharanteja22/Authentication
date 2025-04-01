24235685e78deada1a1c56a35d08d68771defa92
sharanteja2211 <sharanteja2211@gmail.com>
2025-04-01 21:17:22 +0530

    created user upadate api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,36 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
 export const test= (req,res)=>{
     res.json({
         message: "API Is working",
     });
 };
+
+
+export const updateUser = async (req, res,next ) => {
+    if (req.user.id!==req.params.id){
+        return next(errorHandler(403,"You can only update your account"));
+       }
+    try{
+        if (req.body.password){
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,
+            { $set:{
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password 
+            } },
+            { new: true }
+        );
+        const {password,...rest}=updatedUser._doc;
+        if (!updatedUser) {
+            return next(errorHandler(404, "User not found"));
+        } else {
+            res.status(200).json(rest);      
+        }
+    }catch(err){
+        next(err);
+    }
+}
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,6 +14,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app=express();
 
 app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(3000,()=>{
     console.log('server lisining on port 3000')
 })
