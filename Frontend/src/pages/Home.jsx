6ce59e32946f8026ea92d361ba32bb75146c08fe
sharanteja2211 <sharanteja2211@gmail.com>
2025-04-01 23:11:51 +0530

    complete home and about page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-4 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800 text-center'>Welcome to my World...</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat obcaecati ullam quod fuga voluptatem praesentium culpa repellendus eos minima! Dolores esse fugit unde adipisci commodi doloremque nemo quae ea nam in, aliquid ipsa officia laudantium molestiae voluptatibus ullam enim vel incidunt placeat corporis itaque consequuntur, sed sequi? Iure earum esse maxime cupiditate repudiandae optio blanditiis adipisci quibusdam sed quo?</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum excepturi dignissimos dicta facilis error iste distinctio fugiat exercitationem non quibusdam eum explicabo corrupti, perferendis, delectus aliquid labore doloribus blanditiis expedita sunt magni fugit maiores? Consequuntur, soluta omnis aperiam, nisi voluptatum cumque voluptates culpa voluptatem velit corporis atque reiciendis aut praesentium id est officia quibusdam iste perferendis cum! Illo, nesciunt inventore!</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum excepturi dignissimos dicta facilis error iste distinctio fugiat exercitationem non quibusdam eum explicabo corrupti, perferendis, delectus aliquid labore doloribus blanditiis expedita sunt magni fugit maiores? Consequuntur, soluta omnis aperiam, nisi voluptatum cumque voluptates culpa voluptatem velit corporis atque reiciendis aut praesentium id est officia quibusdam iste perferendis cum! Illo, nesciunt inventore!</p>
+
+    </div>
   )
 }
